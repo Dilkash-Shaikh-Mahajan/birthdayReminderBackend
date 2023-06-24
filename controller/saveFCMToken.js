c055b4648bfd73c83359c011f269c25fffa15054
dilkash7 <dilkash.3mindsdigital@gmail.com>
2023-06-25 04:18:38 +0530

    removed

--- controller/saveFCMToken.js
+++ controller/saveFCMToken.js
@@ -3,14 +3,16 @@ const saveFCMToken = require('./../model/saveFCMToken');
 const saveToFCMTokenFunction = async (req, res) => {
 	try {
 		const { fcmToken } = req.body;
-		const alreadyExist = await saveFCMToken.find({ fcmToken });
-
-		const SavedAdminToken = await saveFCMToken.create(req.body);
-		res.status(200).json({
-			message: 'FCM Token Added SuccessFully',
-			data: SavedAdminToken,
-			status: true,
-		});
+		const alreadyExist = await saveFCMToken.findOne({ fcmToken });
+		console.log(alreadyExist);
+		if (alreadyExist.fcmToken != fcmToken) {
+			const SavedAdminToken = await saveFCMToken.create(req.body);
+			res.status(200).json({
+				message: 'FCM Token Added SuccessFully',
+				data: SavedAdminToken,
+				status: true,
+			});
+		}
 	} catch (error) {
 		res.status(403).json({
 			status: false,
