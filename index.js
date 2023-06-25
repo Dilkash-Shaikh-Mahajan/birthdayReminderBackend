7068b9eb4eb361a817e324846fbdd2f8bad7ac94
dilkash7 <dilkash.3mindsdigital@gmail.com>
2023-06-25 14:48:43 +0530

    cors issue removed

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 7786;
 let api = require('./route');
 require('./database');
 app.use(cors());
+app.use((req, res, next) => {
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://birthday-reminder-frontend.vercel.app',
+	);
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
