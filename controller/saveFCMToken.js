67dca2a984d0307881859aac877fa9c8e33b9be9
dilkash7 <dilkash.3mindsdigital@gmail.com>
2023-06-25 14:42:46 +0530

    fcm token already exist code is removed

--- controller/saveFCMToken.js
+++ controller/saveFCMToken.js
@@ -1,25 +1,32 @@
 const saveFCMToken = require('./../model/saveFCMToken');
 
 const saveToFCMTokenFunction = async (req, res) => {
-	try {
-		const { fcmToken } = req.body;
-		const alreadyExist = await saveFCMToken.findOne({ fcmToken });
-		console.log(alreadyExist);
-		if (alreadyExist.fcmToken != fcmToken) {
-			const SavedAdminToken = await saveFCMToken.create(req.body);
-			res.status(200).json({
-				message: 'FCM Token Added SuccessFully',
-				data: SavedAdminToken,
-				status: true,
-			});
-		}
-	} catch (error) {
-		res.status(403).json({
-			status: false,
-			error: error.message,
+	const { fcmToken } = req.body;
+	const alreadyExist = await saveFCMToken.findOne({ fcmToken });
+	console.log(alreadyExist);
+
+	if (alreadyExist === null) {
+		const savedAdminToken = await saveFCMToken.create(req.body);
+		return res.status(200).json({
+			message: 'FCM Token Added Successfully',
+			data: savedAdminToken,
+			status: true,
+		});
+	} else {
+		// Update the existing document with the new token
+		alreadyExist.fcmToken = fcmToken;
+		await alreadyExist.save();
+
+		return res.status(200).json({
+			message: 'FCM Token Updated Successfully',
+			data: alreadyExist,
+			status: true,
 		});
 	}
+
+	// This line will never be reached because the response is already sent
 };
+
 const getAllFCMToken = async (req, res) => {
 	try {
 		const allFCMToken = await saveFCMToken.find();
